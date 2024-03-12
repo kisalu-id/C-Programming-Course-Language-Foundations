c00ba69b487949f23a61cc1f2667486a30f8db98
kisalu-id <152627403+kisalu-id@users.noreply.github.com>
2024-03-12 15:01:53 +0300

    Create Math library.c

---
+++ Other exercises of similar level/Math library.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  //square root of x
+  printf("%f\n", sqrt(16));
+  
+  //cubic root of x
+  printf("%f\n"), cbrt(64);
+  
+  //rounds a number UP to it's nearest integer
+  printf("%f\n", ceil(3.9));
+  
+  //rounds a number DOWN to it's nearest integer
+  printf("%f\n", floor(4.9));
+  
+  //teturns the value of x to the power of y:
+  printf("%f\n", pow(4, 3));
+  
+  return 0;
+}
