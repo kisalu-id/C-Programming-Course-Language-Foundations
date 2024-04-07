81782b189382c843892da47780e9ab619c031a46
kisalu-id <152627403+kisalu-id@users.noreply.github.com>
2024-04-07 21:41:00 +0300

    Update Arrays (readability).c

--- Other exercises of similar level/Arrays (readability).c
+++ Other exercises of similar level/Arrays (readability).c
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
-#include <string.h>
+#include <string.h> //sizeof function
+#include <ctype.h> //isalpha isspace functions
  
 int countLetters (char *);
 int countWords (char *);
@@ -11,7 +12,8 @@ int main() {
     // Prompt the user for some text
     char text[1000];
     printf("Enter your text:\n");
-    scanf("%s", text);
+    fgets(text, (sizeof(text) - 1), stdin);
+    //appends \0 to the strings end, has buffer overflow protecrion, therefore safer
 
     // Count the number of letters, words, and sentences in the text
     int letters = countLetters(text);
@@ -19,35 +21,70 @@ int main() {
     int sentences = countSentences(text);
 
     // Compute the Coleman-Liau index
-    float index = calculateindex(letters, words, sentences);
+    float index = calculateIndex(letters, words, sentences);
 
     // Print the grade level
     int grade = (int)index;
-    if () {
-
-    } else if () {
-
-    } else if () {
+    if (grade <= 1) {
+        printf("Before Grade 1\n");
+    } else if (grade >= 16) {
+        printf("Grade 16+\n");
+    } else if (grade == NULL) {
+        printf("Error... ");
     } else {
-      printf("Error\n");
+        printf("Grade %d", grade);
     }
 
     return 0;
 }
 
 
-int countLetters(char *text) {
+int countLetters(char *word) {
     int count = 0;
-
-
+    for (int i=0; word[i] != '\0'; i++) {
+    	    if (isalpha(word[i])) {
+    	        count ++;
+    	    }
+    	}
     return count;
 }
 
 int count_words(char *word) {
+	   int count = 0;
+    for (int i=0; word[i] != '\0'; i++) {
+    	    if (isspace(word[i])) {
+    	        count ++;
+    	    }
+    	}
+    	return count;
 }
 
-int countSentences(char *text) {}
+int countSentences(char *text) {
+    int count = 0;
+    for (int i=0; text[i] != '\0'; i++) {
+    	    if (text[i] == '.' || text[i] == '!' || text[i] == '?') {
+            count++;
+    	    }
+    	}
+    	return count;
+}
 
-float calculateIndex(int, int, int); {}
+float calculateIndex(int letters, int words, int sentences); {
+    int index = 0;
+    for (int i=0; text[i] != '\0'; i++) {
+    	    
+        
+    	}
+    return 0.0588 * L - 0.296 * S - 15.8;
+}
+
+/*
+L is the average number of letters per 100 words in the text,
+S is the average number of sentences per 100 words in the text
+*/
 
 
+
+// Language:C 
+// Copy the full code and open the CCoder APP to run it. 
+// CCoder APP download link：https://play.google.com/store/apps/details?id=com.ikou.ccoding 
